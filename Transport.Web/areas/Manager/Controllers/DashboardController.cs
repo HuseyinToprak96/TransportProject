321223e6a9033a3bc2ea94362458b3cd96f007cf
Huseyin Toprak <HuseyinToprak96@outlook.com>
2023-07-21 01:39:09 +0300

    Proje oluşturuldu

---
+++ Transport.Web/areas/Manager/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Web.ApiHandler;
+using SocialMedia.Web.Helpers;
+using SocialMedia.Web.Models.Dtos;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Web.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    public class DashboardController : Controller
+    {
+        private readonly RestApiHandler _restApiHandler;
+
+        public DashboardController(RestApiHandler restApiHandler)
+        {
+            _restApiHandler = restApiHandler;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Token = CookieHelper.GetCookieValue(HttpContext, "Token");
+            var newUsersResult = await _restApiHandler.GetAsync<CustomResponseDto<IEnumerable<UserAppDto>>>("ManagerUser/GetUsersNotShow", Token);
+            var resultShowUser = await _restApiHandler.PostAsync <NoDataDto,CustomResponseDto<NoDataDto>>("ManagerUser/Show", new NoDataDto(),Token);
+            var resultShowShared = await _restApiHandler.PostAsync<NoDataDto, CustomResponseDto<NoDataDto>>("ManagerShared/Show", new NoDataDto(), Token);
+
+            return View(Tuple.Create(newUsersResult.Data));
+        }
+    }
+}
---
+++ Transport.Web/areas/Manager/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Web.ApiHandler;
+using SocialMedia.Web.Filters;
+using SocialMedia.Web.Helpers;
+using SocialMedia.Web.Models.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Web.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    [AllowAnonymous]
+    [LoginFilter]
+    public class UserController : Controller
+    {
+        private readonly RestApiHandler _restApiHandler;
+
+        public UserController(RestApiHandler restApiHandler)
+        {
+            _restApiHandler = restApiHandler;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var Token = CookieHelper.GetCookieValue(HttpContext, "Token");
+            var result = await _restApiHandler.GetAsync<CustomResponseDto<List<UserAppDto>>>("ManagerUser/GetAll",Token);
+            return View(result.Data);
+        }
+    }
+}
